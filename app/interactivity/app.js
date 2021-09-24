f982b2fd9a47466df69a68fbeb7dfbc4944ba027
Julia Hoarau <julia_hoarau@outlook.com>
2021-09-24 14:51:05 +1000

    Tweaked fetch methods to ensure API requests return 200 OK

--- app/interactivity/app.js
+++ app/interactivity/app.js
@@ -1,1 +1,118 @@
 console.log('todo app')
+
+function login (evt) {
+    evt.preventDefault()
+    console.log(evt)
+    var theFormData = {};
+    for (i=0;i<evt.target.length;i++) {
+        theFormData[evt.target[i].name]  =
+        evt.target[i].value;
+    }
+
+    fetch(evt.target.action, 
+        {
+            method: 'POST',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+}
+
+function register (evt) {
+    evt.preventDefault()
+    console.log(evt)
+    var theFormData = {};
+    for (i=0;i<evt.target.length;i++) {
+        theFormData[evt.target[i].name]  =
+        evt.target[i].value;
+    }
+
+    fetch(evt.target.action, 
+        {
+            method: 'PUT',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+}
+
+//DOESN'T WORK
+function userexists (evt) {
+    evt.preventDefault()
+    url = evt.target.action + '?username=' + evt.target[0].value;
+    fetch(url, 
+        {
+            method: 'GET',
+            credentials: 'include',
+        })
+}
+
+function userloggedin (evt) {
+    evt.preventDefault()
+    fetch(evt.target.action,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+      )
+}
+
+function userlogout (evt) {
+    evt.preventDefault()
+    fetch(evt.target.action,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+      )
+}
+
+
+function getcategories (evt) {
+    evt.preventDefault()
+    fetch(evt.target.action,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+      )
+}
+function addcategory (evt) {
+    evt.preventDefault()
+    method = {method: evt.target.method}
+    fetch(evt.target.action, method)
+}
+
+function updatecategory (evt) {
+    evt.preventDefault()
+    method = {method: evt.target.method}
+    fetch(evt.target.action, method)
+}
+
+function gettodos (evt) {
+    evt.preventDefault()
+    method = {method: evt.target.method}
+    fetch(evt.target.action, method)
+}
+
+function addtodo(evt) {
+    evt.preventDefault()
+    method = {method: evt.target.method}
+    fetch(evt.target.action, method)
+}
+
+function deltodo(evt) {
+    evt.preventDefault()
+    method = {method: evt.target.method}
+    fetch(evt.target.action, method)
+}
+
+function updatetodo(evt) {
+    evt.preventDefault()
+    method = {method: evt.target.method}
+    fetch(evt.target.action, method)
+}
--- server.js
+++ server.js
@@ -3,14 +3,67 @@ const app = express()
 
 app.use('/', express.static('app'))
 
-app.get('/', function (req, res) {
-  res.send('Test Page')
+
+// Authentication
+app.post('/api/login', function (req, res) {
+  res.send('Test login')
 })
 
-app.get('/api', function (req, res) {
-    res.send('Test the API')
-  })
+app.put('/api/register', function (req, res) {
+    res.send('Test register')
+})
 
-app.listen(3000)
+app.get('/api/isLoggedin', function (req, res) {
+    res.send('Test isLoggedin')
+})
+
+app.get('/api/doesuserexist', function (req, res) {
+    res.send('Test does user exist')
+})
+
+app.get('/api/logout', function (req, res) {
+    res.send('Test logout')
+})
+
+//Todo categories CRUD
+app.get('/api/getcat', function (req, res) {
+    res.send('Test get categories')
+})
+
+app.put('/api/addcat', function (req, res) {
+    res.send('Test add categories')
+})
+
+app.delete('/api/delcat/:catid', function (req, res) {
+    res.send('Test delete categories')
+})
+
+app.patch('/api/updatecat/:catid', function (req, res) {
+    res.send('Test update categories')
+})
+
+
+//Todo list CRUD
+app.get('/api/gettodos/:catid', function (req, res) {
+    res.send('Test the API')
+})
+
+app.put('/api/addtodo', function (req, res) {
+    res.send('Test the API')
+})
+
+app.delete('/api/deltodo/:todoid', function (req, res) {
+    res.send('Test the API')
+})
+
+app.patch('/api/updatetodo/:todoid', function (req, res) {
+    res.send('Test the API')
+})
+
+
+
+
+
+app.listen(5000)
 
 
